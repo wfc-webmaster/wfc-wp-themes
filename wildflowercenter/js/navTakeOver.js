270a4ffc724feea739906b37a17ef3f5b9541fbd
Elden <webmaster@wildflower.org>
2015-07-17 16:30:57 -0500

    Takeover Nav fun times

--- wildflowercenter/js/navTakeOver.js
+++ wildflowercenter/js/navTakeOver.js
@@ -1,4 +1,5 @@
 // Adds a wrapping <div> around both navs to center them but allow the contents to be right aligned
+
 var actionNav = document.getElementById('menu-action-nav').outerHTML,
 actionNavWrap = '<div id="menu-action-nav-wrap">' + actionNav + '</div>';
 
@@ -12,114 +13,101 @@ var mainNav = document.getElementById('menu-main-nav'),
 document.getElementById('menu-main-nav').outerHTML = mainNavWrap;
 
 //Begin Take-Over nav
-var mainNavSection = document.getElementById('wrapper-wyt55789b870b1d1');
-	mainNavSection.outerHTML += '<div id="takeover-visit" class="takeover-show">One</div><div id="takeover-plants" class="takeover-show">Two</div>';
-
 
 var takeoverContainer = document.getElementById('takeover-container'),
 	navVisit = document.getElementById('menu-item-1779'),
-	wfcGray = "#4d4d4d",
-	wfcGreen = "#7ec23b";
+	navPlants = document.getElementById('menu-item-1780'),
+	navLearn = document.getElementById('menu-item-1781'),
+	navWork = document.getElementById('menu-item-1782'),
+	navNews = document.getElementById('menu-item-1783');
 
-	// navVisit.addEventListener("mouseover", showTakeover);
-	// navVisit.addEventListener("mouseout", hideTakeover);
-	// //takeoverContainer.addEventListener("mouseover", testFun);
-	// takeoverContainer.addEventListener("mouseout", hideTakeover);
+var codeVisit = [
+	'<div id="visit-takeover" class="takeover-show">',
+		'<div id="visit-takeover-wrap" class="visit-wrap">',
+			'<div class="subnav-list-wrap">',
+			'<ul class="subnav-list">',
+				'<li>Item 1</li>',
+				'<li>Item 2</li>',
+				'<li>Item 3</li>',
+			'</ul>',
+			'</div>',
+			'<div class="subnav-images">',
+			'</div>',
+		'</div>',
+	'</div>'
+].join('');
+
+var codePlants = [
+	'<div id="plants-takeover" class="takeover-show">',
+		'<div id="plants-takeover-wrap" class="visit-wrap">',
+			'<div class="subnav-list-wrap">',
+			'<ul class="subnav-list">',
+				'<li>Item 4</li>',
+				'<li>Item 5</li>',
+				'<li>Item 6</li>',
+			'</ul>',
+			'</div>',
+		'</div>',
+	'</div>'
+].join('');
+
+var codeLearn = [
+	'<div id="learn-takeover" class="takeover-show">',
+		'<div id="learn-takeover-wrap" class="visit-wrap">',
+			'<div class="subnav-list-wrap">',
+			'<ul class="subnav-list">',
+				'<li>Item 7</li>',
+				'<li>Item 8</li>',
+				'<li>Item 9</li>',
+			'</ul>',
+			'</div>',
+		'</div>',
+	'</div>'
+].join('');
+
+var codeWork = [
+	'<div id="work-takeover" class="takeover-show">',
+		'<div id="work-takeover-wrap" class="visit-wrap">',
+			'<div class="subnav-list-wrap">',
+			'<ul class="subnav-list">',
+				'<li>Item 10</li>',
+				'<li>Item 11</li>',
+				'<li>Item 12</li>',
+			'</ul>',
+			'</div>',
+		'</div>',
+	'</div>'
+].join('');
+
+var codeNews = [
+	'<div id="news-takeover" class="takeover-show">',
+		'<div id="news-takeover-wrap" class="visit-wrap">',
+			'<div class="subnav-list-wrap">',
+			'<ul class="subnav-list">',
+				'<li>Item 13</li>',
+				'<li>Item 14</li>',
+				'<li>Item 15</li>',
+			'</ul>',
+			'</div>',
+		'</div>',
+	'</div>'
+].join('');
 	
-	// function showTakeover() {
-	// 	takeoverContainer.style.display = "table";
-	// 	this.style.backgroundColor = wfcGreen;
-	// }
-
-	// function testFun() {
-	// 	navVisit.style.backgroundColor = "orange";
-	// 	console.log('You moused over.');
-	// }
-
-	// function hideTakeover() {
-	// 	takeoverContainer.style.display = "none";
-	// 	navVisit.style.backgroundColor = wfcGray;
-	// }
-
-	
-// jQuery(document).ready(function($) {
-// 	var navTimeout;
-
-// 	// $(navVisit).hover(function() {
-// 	// 	$(this).css("background-color", wfcGreen);
-// 	// 	$(takeoverContainer).show();
-// 	// 	//console.log('You hovered over it.');
-// 	// },
-
-// 	// function() {
-// 	// 	navTimeout = window.setTimeout(function() {
-// 	// 		$(navVisit).css("background-color", wfcGray);
-// 	// 		$(takeoverContainer).hide();
-// 	// 	}, 200);
-// 	// });
-
-// navHover($('#menu-item-1779 a'));
-// navHover($('#menu-item-1780 a'));
-// navHover($('#menu-item-1781 a'));	
-
-// 	$(takeoverContainer).hover(function() {
-// 		clearTimeout(navTimeout);
-// 		$(this).show;
-// 	},
-// 	function() {
-// 		setTimeout(function() {
-// 			$(navVisit).css("background-color", wfcGray);
-// 			$(takeoverContainer).hide();				
-// 		}, 200);
-// 	}
-// 	);
-
-// function navHover(navName) {
-// 	navName.hover(function(){
-// 		clearTimeout(navTimeout);
-// 		$(navName).css("background-color", wfcGreen);
-// 		$(takeoverContainer).show();
-//     },
-
-//     function() {
-// 		navTimeout = setTimeout(function() {
-// 			$(navName).css("background-color", wfcGray);
-// 			$(takeoverContainer).hide();
-// 		}, 200);
-// 	});
-// }
-
-// });
+navVisit.innerHTML += codeVisit;
+// navPlants.innerHTML += codePlants;
+// navLearn.innerHTML += codeLearn;
+// navWork.innerHTML += codeWork;
+// navNews.innerHTML += codeNews;	
 
 jQuery(document).ready(function($) {
 
-var timeout; // store a timeout here
+	$('#menu-main-nav > li').hover(function() {
+		$(this).addClass('active-nav');
+		$(this).children('.takeover-show').css('display', 'table');
+	},
+	 function() {    		
+		$('.takeover-show').hide();
+		$(this).removeClass('active-nav');
+	});
 
-$('#menu-main-nav li a').hover(function() {
-    clearTimeout(timeout);
-    $('.takeover-show').hide().eq($(this).parent().index()).show();
-}, 
-
-    function() {
-    timeout = setTimeout(function() {
-        $('.takeover-show').hide();
-    }, 1000);
 });
-
-
-$('.takeover-show').hover(function() {
-    clearTimeout(timeout);
-    $('.takeover-show').hide();
-    $(this).show();
-}, 
-
-    function() {
-    timeout = setTimeout(function() {
-        $('.takeover-show').hide();
-    }, 1000);
-});
-});
-
-
-
-
