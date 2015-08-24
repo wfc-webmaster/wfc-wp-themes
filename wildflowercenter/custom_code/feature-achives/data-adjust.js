f5efb482c88740a2dd869efc8972ca56b452bffd
Elden <webmaster@wildflower.org>
2015-08-24 17:06:01 -0500

    Completed AngularJS directive

---
+++ wildflowercenter/custom_code/feature-achives/data-adjust.js
@@ -0,0 +1,7 @@
+angular.element(document).ready(function() {
+	var yoyo = angular.element('div');
+	console.log(yoyo);
+	console.log('Document is ready.');
+});
+
+console.log('Document.');
--- wildflowercenter/custom_code/feature-achives/data.js
+++ wildflowercenter/custom_code/feature-achives/data.js
@@ -1,8 +1,28 @@
 var app = angular.module("featureArchive", ['ngSanitize']);
 
-app.controller("getFeatures", function($scope, $http){
+app.controller("getFeatures", function($scope, $http, $timeout){
 	$http.get('http://localhost:8888/wildflower_2/wordpress/wp-content/themes/wildflowercenter/custom_code/feature-achives/api.php').success(function(data) {
             // here the data from the api is assigned to a variable named users
-            $scope.features = data;
+        $scope.features = data;
     });
 });
+
+app.directive('fullArticle', function($compile) {
+	return {
+		restrict: 'E',
+		link: function($scope, element, attr) {
+			// Prepend feature article links so they go to wildflower.org pages
+			featureLink = $('a[href^="/plants"]');
+			$(featureLink).attr('href', function(i,v) {
+    			return "http://wildflower.org" + v;
+			});
+			// Prepend feature article images so they display
+			// Need to change when images are migrated to permanent home
+			featureImg = $('img[src^="/_images"');
+			$(featureImg).attr('src', function(i,s) {
+				return "http://wildflower.org" + s;
+			});
+			console.log('Still works');			
+		}
+	};
+})
