e06ef905edf236e52e27aceebc211b15a5626885
Elden <webmaster@wildflower.org>
2015-10-15 17:02:52 -0500

    Social media sharebar added

--- wildflowercenter/functions.php
+++ wildflowercenter/functions.php
@@ -16,4 +16,31 @@ function add_categories_above_title() {
 
 add_action('headway_before_entry_title', 'add_categories_above_title');
 
+function insert_tumblr_script() {
+	echo '<script type="text/javascript" src="http://platform.tumblr.com/v1/share.js"></script>';
+}
+
+function add_sharebar_to_posts() {
+	if ( is_single() ) {		
+		add_action('wp_footer', 'insert_tumblr_script');
+	?>
+	<div class="post-sharebar-wrapper">
+		<ul id="post-sharebar">
+			<li><a href="mailto:?subject=Wildflower Center article: <?php echo ucwords(get_the_title()); ?>&body=I read this and wanted to share: <?php echo ucwords(get_the_title()); ?> (<?php echo urlencode(the_permalink()); ?>)" title="E-Mail this"><i class="fa fa-envelope sharebar-icons"></i>E-Mail</a></li>
+			<li><a href="https://www.facebook.com/sharer.php?u=<?php the_permalink();?>&amp;t=<?php the_title(); ?>" title="Share on Facebook" target="_blank"><i class="fa fa-facebook sharebar-icons"></i>Facebook</a></li>
+			<li><a href="https://twitter.com/home/?status=<?php the_title(); ?> - <?php the_permalink(); ?>" title="Tweet this" target="_blank"><i class="fa fa-twitter sharebar-icons"></i>Twitter</a></li>
+			<li><a href="https://www.linkedin.com/shareArticle?mini=true&amp;title=<?php the_title(); ?>&amp;url=<?php the_permalink(); ?>" title="Share on LinkedIn" target="_blank"><i class="fa fa-linkedin sharebar-icons"></i>LinkedIn</a></li>
+			<li><a href="https://plus.google.com/share?url=<?php the_permalink(); ?>" target="_blank"><i class="fa fa-google-plus sharebar-icons"></i>Google+</a></li>
+			<li><a href="https://www.tumblr.com/share/" title="Share on Tumblr" target="_blank"><i class="fa fa-tumblr sharebar-icons"></i>Tumblr</a></li>
+		</ul>
+	</div>
+	<?php
+	}
+}
+
+add_action('headway_before_entry_content', 'add_sharebar_to_posts');
+add_action('headway_after_entry_content', 'add_sharebar_to_posts');
+
+
+
 ?>
